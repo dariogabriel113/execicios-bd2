78699e0f17de9b8d148ace0c591f0413aa793936
Dario Gabriel <dariogabriel113@gmail.com>
2017-09-16 18:04:58 -0300

    Add resolucao

--- Exercicio01/WebContent/WebContent/js/manipulaArquivo.js
+++ Exercicio01/WebContent/WebContent/js/manipulaArquivo.js
@@ -24,48 +24,79 @@ function leCSV(evt) {
 
 	var fileArr = evt.target.result.split('\n');
 	var strDiv = '<table>';
+	var posicao;
+	var qtdColunas;
 
 	for ( var i = 0; i < fileArr.length; i++) {
 		strDiv += '<tr>';
 		var fileLine = fileArr[i].split(';');
+		if (i == 0) {
+			qtdColunas = fileLine.length;
+		};
 		for ( var j = 0; j < fileLine.length; j++) {
 			strDiv += '<td>' + fileLine[j].trim() + '</td>';
+			if (fileLine[j].trim() == 'saldo') {
+				posicao = j;
+			}
+			if (fileLine[posicao] != 'saldo' && fileLine[posicao] != NaN && fileLine[posicao] != undefined) {
+				totalArquivoTipo1 = parseInt(totalArquivoTipo1) + parseInt(fileLine[posicao]);
+			}
 		}
 		strDiv += '</tr>';
 	}
+	totalArquivoTipo1 = totalArquivoTipo1/qtdColunas;
 	// Imprime total 2
 	strDiv += '<tr>';
 	strDiv += '<td>Total</td>';
 	strDiv += '<td>' + totalArquivoTipo1 + '</td>';
+	strDiv += '<td>' + qtdColunas + '</td>';
 	strDiv += '</tr>';
 
 	strDiv += '</table>';
 
 	var CSVsaida = document.getElementById('CSVsaida');
 	CSVsaida.innerHTML = strDiv;
+
+	totalArquivoTipo1 = 0
 }
 
 function leCSV2(evt) {
 
 	var fileArr = evt.target.result.split('\n');
 	var strDiv = '<table>';
+	var posicao;
+	var qtdColunas;
 
 	for ( var i = 0; i < fileArr.length; i++) {
 		strDiv += '<tr>';
 		var fileLine = fileArr[i].split(';');
+		if (i == 0) {
+			qtdColunas = fileLine.length;
+		};
 		for ( var j = 0; j < fileLine.length; j++) {
 			strDiv += '<td>' + fileLine[j].trim() + '</td>';
+			if (fileLine[j].trim() == 'saldo') {
+				posicao = j;
+			}
+			if (fileLine[posicao] != 'saldo' && fileLine[posicao] != NaN && fileLine[posicao] != undefined) {
+				totalArquivoTipo2 = parseInt(totalArquivoTipo2) + parseInt(fileLine[posicao]);
+			}
 		}
 		strDiv += '</tr>';
 	}
+	
 
+	totalArquivoTipo2 = totalArquivoTipo2/qtdColunas;
 	// Imprime total 2
 	strDiv += '<tr>';
 	strDiv += '<td>Total</td>';
 	strDiv += '<td>' + totalArquivoTipo2 + '</td>';
+	strDiv += '<td>' + qtdColunas + '</td>';
 	strDiv += '</tr>';
 	strDiv += '</table>';
 
 	var CSVsaida = document.getElementById('CSVsaida2');
 	CSVsaida.innerHTML = strDiv;
+
+	totalArquivoTipo2 = 0
 }
